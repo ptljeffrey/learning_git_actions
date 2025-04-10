7dbc22fc791b69999651a542bd118198a422c118
ptljeffrey <jeffreymedwin.r@pointelsolutions.com>
2025-04-10 16:00:12 +0530

    find timezone

--- datetimefile.py
+++ datetimefile.py
@@ -1,6 +1,6 @@
 # Import the 'datetime' module to work with date and time
 import datetime
-from time import gmtime, strftime
+from time import tzname
 
 
 # Get the current date and time
@@ -9,8 +9,7 @@ now = datetime.datetime.now()
 # Create a datetime object representing the current date and time
 
 # Display a message indicating what is being printed
-print("Current date and time : ")
-
 # Print the current date and time in a specific format
-print(now.strftime("%Y-%m-%d %H:%M:%S"))
-print(strftime("Time Zone: %z", gmtime()))
+print("Current date and time : ", now.strftime("%Y-%m-%d %H:%M:%S"))
+
+print("time zone: ", tzname)
